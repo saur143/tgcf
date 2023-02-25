d2e4e52ba2a8c0a41ee50e1c27135a72dd45a832
saur143 <53489864+saur143@users.noreply.github.com>
2023-02-25 16:22:34 +0530

    Update replace.py

--- tgcf/plugins/replace.py
+++ tgcf/plugins/replace.py
@@ -1,7 +1,12 @@
-import logging
-from typing import Any, Dict
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 import requests
 from urllib.parse import urlparse, parse_qs
+import logging
+from typing import Any, Dict
+import re
 
 from pydantic import BaseModel  # pylint: disable=no-name-in-module
 
@@ -21,34 +26,41 @@ class TgcfReplace(TgcfPlugin):
         msg_text: str = tm.text
         if not msg_text:
             return tm
-        msg_text = re.findall("(?P<url>https?://[^\s]+)", msg_text)
-        for link in msg_text:
-            try:
-                # follow redirects to get the final URL
-                response = requests.head(link, allow_redirects=True)
-                final_url = response.url
-                
-                # parse the URL to get the domain and query parameters
-                parsed_url = urlparse(final_url)
-                domain = parsed_url.netloc
-                query_params = parse_qs(parsed_url.query)
-                
-                # add the affid tag if the domain is flipkart or amazon
-                if domain == "www.flipkart.com":
-                    query_params["affid"] = "saurabhpp1"
-                elif domain == "www.amazon.in":
-                    query_params["tag"] = "lootdealsfree-21"
-                
-                # construct the modified URL
-                modified_url = f"{parsed_url.scheme}://{domain}{parsed_url.path}"
-                if query_params:
-                    modified_url += "?" + "&".join([f"{key}={value[0]}" for key, value in query_params.items()])
-                
-                # replace the original link with the modified link in the message text
-                msg_text = msg_text.replace(link, modified_url)
-            except:
-                # if there's an error with the link, just move on to the next one
-                pass
-                
+        for link in links:
+        	try:
+        		# follow redirects to get the final URL
+        		options = Options()
+        		options.add_argument('--headless')
+        		options.add_argument('--no-sandbox')
+        		options.add_argument('--disable-dev-shm-usage')
+        		driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+        
+        		driver.get(link)
+        		final_url = driver.current_url
+        
+        		driver.close()
+        		# parse the URL to get the domain and query parameters
+        		# parse the URL to get the domain and query parameters
+        		parsed_url = urlparse(final_url)
+        		domain = parsed_url.netloc
+        		query_params = parse_qs(parsed_url.query)
+        		# add the affid tag if the domain is flipkart or amazon
+        		if domain == "www.flipkart.com":
+        			query_params["affid"] = "saurabhpp1"
+        		elif domain == "www.amazon.in":
+        			query_params["tag"] = 'lootdealsfree-21'
+        		# construct the modified URL
+        		modified_url = f"{parsed_url.scheme}://{domain}{parsed_url.path}"
+        		if query_params:
+        			modified_url += "?" + "&".join([f"{key}={value}" for key, value in query_params.items()])
+        		#shorten url
+        		
+        			
+        
+        		# replace the original link with the modified link in the message text
+        		msg_text = msg_text.replace(link, modified_url)
+        	except:
+        		# if there's an error with the link, just move on to the next one
+        		pass
         tm.text = msg_text
         return tm
